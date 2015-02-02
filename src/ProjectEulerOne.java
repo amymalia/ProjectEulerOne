ab77df643ea37d0ab1392834795f483e10b9772a
amymalia <amytakayesu@gmail.com>
2015-02-02 02:51:35 -1000

    Finished code
    Finished code.

---
+++ src/ProjectEulerOne.java
@@ -0,0 +1,14 @@
+/**
+ * Created by amytakayesu on 2/2/15.
+ */
+public class ProjectEulerOne {
+  public static void main(String[] args) {
+    int sum = 0;
+    for(int i = 0; i < 1000; i++){
+      if((i % 3 == 0) || (i % 5 == 0)){
+        sum += i;
+      }
+    }
+    System.out.println("Sum is " + sum);
+  }
+}
